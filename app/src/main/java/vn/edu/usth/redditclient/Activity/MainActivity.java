878ba034dac74a871a1e82786a25de6924ae675d
miatran1109 <miatran1109@gmail.com>
2021-01-24 03:24:47 +0700

    Add onClick for vote-up, vote-down, gift

--- app/src/main/java/vn/edu/usth/redditclient/Activity/MainActivity.java
+++ app/src/main/java/vn/edu/usth/redditclient/Activity/MainActivity.java
@@ -29,4 +29,46 @@ public class MainActivity extends AppCompatActivity {
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
     }
+
+    //Manage buttons
+    //Vote-up, vote-down
+    boolean clicked = true;
+    public void voteUp(View view){
+        ImageView img = (ImageView) findViewById(R.id.vote_up_button);
+        if (clicked) {
+            clicked = false;
+            img.setImageResource(R.drawable.vote_up_red);
+            Toast.makeText(getApplicationContext(), "Voted up", Toast.LENGTH_SHORT).show();
+        }else{
+            clicked = true;
+            img.setImageResource(R.drawable.long_arrow_alt_up_solid);
+            Toast.makeText(getApplicationContext(), "Un-voted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void voteDown(View view){
+        ImageView img = (ImageView) findViewById(R.id.vote_down_button);
+        if (clicked) {
+            clicked = false;
+            img.setImageResource(R.drawable.vote_down_blue);
+            Toast.makeText(getApplicationContext(), "Voted down", Toast.LENGTH_SHORT).show();
+        }else{
+            clicked = true;
+            img.setImageResource(R.drawable.long_arrow_alt_down_solid);
+            Toast.makeText(getApplicationContext(), "Un-voted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void gift(View view){
+        ImageView img = (ImageView) findViewById(R.id.gift_button);
+        if (clicked) {
+            clicked = false;
+            img.setImageResource(R.drawable.gift_red);
+            Toast.makeText(getApplicationContext(), "Gift sent", Toast.LENGTH_SHORT).show();
+        }else{
+            clicked = true;
+            img.setImageResource(R.drawable.gift_solid);
+            Toast.makeText(getApplicationContext(), "Take back :<", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
