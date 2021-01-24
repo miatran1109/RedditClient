55879344ad74bdfabbf33ca86c8eb486306a5f45
miatran1109 <miatran1109@gmail.com>
2021-01-24 18:05:04 +0700

    Display interface for share and comment.

---
+++ app/src/main/java/vn/edu/usth/redditclient/Activity/DisplayCommentActivity.java
@@ -0,0 +1,16 @@
+package vn.edu.usth.redditclient.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.edu.usth.redditclient.R;
+
+public class DisplayCommentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_comment);
+    }
+}
---
+++ app/src/main/java/vn/edu/usth/redditclient/Activity/ShareActivity.java
@@ -0,0 +1,15 @@
+package vn.edu.usth.redditclient.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import vn.edu.usth.redditclient.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share2);
+    }
+}
